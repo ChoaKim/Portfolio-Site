d2a667d839c3ed762e45a36e2b7cb779dc7315ab
ChoaKim <choa.linzy@gmail.com>
2023-11-23 19:06:13 +0000

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,40 @@ function menuToggle() {
       x.className = 'navtoggle';
     }
   }
+
+  $(document).ready(function() {
+    $('.slideshow-image').click(function() {
+        var $current = $(this);
+        var $next = $current.next('.slideshow-image');
+        if ($next.length === 0) {
+            $next = $('.slideshow-image').first();
+        }
+        $current.addClass('hidden');
+        $next.removeClass('hidden');
+    });
+});
+
+// Slideshow auto play
+// $(document).ready(function() {
+//   // Function to switch to the next image
+//   function showNextImage() {
+//       var $current = $('.slideshow-image:not(.hidden)');
+//       var $next = $current.next('.slideshow-image');
+      
+//       if ($next.length === 0) {
+//           $next = $('.slideshow-image').first();
+//       }
+      
+//       $current.addClass('hidden');
+//       $next.removeClass('hidden');
+//   }
+
+//   // Set an interval to switch images every 5 seconds (5000 milliseconds)
+//   var slideshowInterval = setInterval(showNextImage, 4000);
+
+//   // Stop the interval when an image is clicked
+//   $('.slideshow-image').click(function() {
+//       clearInterval(slideshowInterval);
+//       showNextImage(); // Show the next image immediately when clicked
+//   });
+// });
